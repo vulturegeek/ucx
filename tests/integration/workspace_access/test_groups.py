dbaf1dde9eb625b1f07bdfa3bf0394ac8a70eba4
FastLee <lbareket@gmail.com>
2023-11-27 11:26:48 -0500

    Reintroduce Cluster Policy test

--- tests/integration/workspace_access/test_groups.py
+++ tests/integration/workspace_access/test_groups.py
@@ -118,20 +118,41 @@ def test_delete_ws_groups_should_not_delete_non_reflected_acc_groups(ws, make_uc
 
 
 @retried(on=[NotFound, TimeoutError, AssertionError], timeout=timedelta(minutes=15))
-def test_replace_workspace_groups_with_account_groups(ws, sql_backend, inventory_schema, make_ucx_group, make_table):
+def test_replace_workspace_groups_with_account_groups(
+    ws,
+    sql_backend,
+    inventory_schema,
+    make_ucx_group,
+    make_cluster_policy,
+    make_cluster_policy_permissions,
+    make_table,
+):
     ws_group, _ = make_ucx_group()
-    logger.info("test_replace_workspace_groups_with_account_groups() Testing setting permissions on a database object.")
-    dummy_table = make_table()
-    logger.info(f"Table name {dummy_table.full_name} group name {ws_group.display_name}")
-    sql_backend.execute(f"GRANT SELECT, MODIFY ON TABLE {dummy_table.full_name} TO `{ws_group.display_name}`")
-    sql_backend.execute(f"ALTER {dummy_table.full_name} OWNER TO `{ws_group.display_name}`")
-
-    group_manager = GroupManager(sql_backend, ws, inventory_schema, [ws_group.display_name], "ucx-temp-")
+    cluster_policy = make_cluster_policy()
+    make_cluster_policy_permissions(
+        object_id=cluster_policy.policy_id,
+        permission_level=PermissionLevel.CAN_USE,
+        group_name=ws_group.display_name,
+    )
+    logger.info(f"Cluster policy: {ws.config.host}#setting/clusters/cluster-policies/view/{cluster_policy.policy_id}")
 
     tables = TablesCrawler(sql_backend, inventory_schema)
     grants = GrantsCrawler(tables)
+
+    dummy_table = make_table()
+    sql_backend.execute(f"GRANT SELECT, MODIFY ON TABLE {dummy_table.full_name} TO `{ws_group.display_name}`")
+    sql_backend.execute(f"ALTER {dummy_table.full_name} OWNER TO `{ws_group.display_name}`")
+    res = grants.for_table_info(dummy_table)
+    assert len(res[ws_group.display_name]) == 3
+
+    group_manager = GroupManager(sql_backend, ws, inventory_schema, [ws_group.display_name], "ucx-temp-")
+
+    generic_permissions = GenericPermissionsSupport(
+        ws, [Listing(ws.cluster_policies.list, "policy_id", "cluster-policies")]
+    )
+
     tacl = TableAclSupport(grants, sql_backend)
-    permission_manager = PermissionManager(sql_backend, inventory_schema, [tacl])
+    permission_manager = PermissionManager(sql_backend, inventory_schema, [generic_permissions, tacl])
 
     permission_manager.inventorize_permissions()
 
@@ -145,7 +166,7 @@ def test_replace_workspace_groups_with_account_groups(ws, sql_backend, inventory
     assert "OWN" in table_permissions[ws_group.display_name]
 
     state = group_manager.get_migration_state()
-    assert len(state.groups) == 1
+    assert len(state) == 1
 
     group_manager.rename_groups()
 
@@ -157,9 +178,13 @@ def test_replace_workspace_groups_with_account_groups(ws, sql_backend, inventory
 
         table_permissions = grants.for_table_info(dummy_table)
         assert group_info.name_in_workspace not in table_permissions
+        assert group_info.temporary_name in table_permissions
         assert "MODIFY" in table_permissions[group_info.temporary_name]
         assert "SELECT" in table_permissions[group_info.temporary_name]
         assert "OWN" in table_permissions[group_info.temporary_name]
+
+        policy_permissions = generic_permissions.load_as_dict("cluster-policies", cluster_policy.policy_id)
+        assert PermissionLevel.CAN_USE == policy_permissions[group_info.temporary_name]
 
     check_permissions_for_backup_group()
 
@@ -171,9 +196,14 @@ def test_replace_workspace_groups_with_account_groups(ws, sql_backend, inventory
 
         table_permissions = grants.for_table_info(dummy_table)
         assert group_info.name_in_account not in table_permissions
+        assert group_info.temporary_name in table_permissions
         assert "MODIFY" in table_permissions[group_info.temporary_name]
         assert "SELECT" in table_permissions[group_info.temporary_name]
         assert "OWN" in table_permissions[group_info.temporary_name]
+
+        policy_permissions = generic_permissions.load_as_dict("cluster-policies", cluster_policy.policy_id)
+        assert group_info.name_in_workspace not in policy_permissions
+        assert PermissionLevel.CAN_USE == policy_permissions[group_info.temporary_name]
 
     check_permissions_after_replace()
 
@@ -192,6 +222,10 @@ def test_replace_workspace_groups_with_account_groups(ws, sql_backend, inventory
         assert "SELECT" in table_permissions[group_info.name_in_account]
         assert "OWN" in table_permissions[group_info.name_in_account]
 
+        policy_permissions = generic_permissions.load_as_dict("cluster-policies", cluster_policy.policy_id)
+        assert PermissionLevel.CAN_USE == policy_permissions[group_info.name_in_account]
+        assert PermissionLevel.CAN_USE == policy_permissions[group_info.temporary_name]
+
     check_permissions_for_account_group()
 
     group_manager.delete_original_workspace_groups()
@@ -200,9 +234,11 @@ def test_replace_workspace_groups_with_account_groups(ws, sql_backend, inventory
     def check_table_permissions_after_backup_delete():
         logger.info("check_table_permissions_after_backup_delete()")
 
+        policy_permissions = generic_permissions.load_as_dict("cluster-policies", cluster_policy.policy_id)
+        assert group_info.temporary_name not in policy_permissions
+
         table_permissions = grants.for_table_info(dummy_table)
         assert group_info.name_in_account in table_permissions
-        assert group_info.temporary_name not in table_permissions
         assert "MODIFY" in table_permissions[group_info.name_in_account]
         assert "SELECT" in table_permissions[group_info.name_in_account]
         assert "OWN" in table_permissions[group_info.name_in_account]
